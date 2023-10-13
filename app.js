ef3386da3ddba07b9cc5658e075911b6435f52e2
HongMoonGI <hmoongi94@gmail.com>
2023-10-13 16:50:24 +0900

    서버 고장

--- app.js
+++ app.js
@@ -1,12 +1,18 @@
 const http = require("http")
 const fs = require("fs");
+const { request } = require("http");
 
 const contentType = {
   'Content-Type': 'text/html',
   'charset': 'utf-8',
 }
+
 const contentimgType = {
-  'Content-Type' : 'image/apng',
+  'Content-Type' : 'image/jpeg',
+}
+
+const contentimgType2 = {
+  'Content-Type' : 'image/jpng',
 }
 
 
@@ -17,33 +23,59 @@ const server = http.createServer(function(request,response){
   }
   
   if(request.url === "/hmoongi94"){
-    // console.log(request)
-
-    // response.writeHead(200, contentimgType);
-    // fs.readFile("./static/image/디그다.png", function(err,data){
-    //   if(err){
-    //     console.log('이미지 파일을 읽지 못했습니다.')
-    //   } else{
-    //     response.end(data)
-    //   }
-    // })
-
-    response.writeHead(200, contentType);
     fs.readFile("./static/index.html", "utf-8", function(err, data){
       if(err){
         console.log('파일을 읽지 못했습니다.')
       } else {
+        response.writeHead(200, contentType)
         response.end(data)
-        // image파일이 안불러와짐
       }
     })
   }
   
-  else{
+
+
+
+if(request.url === "/hmoongi94image"){
+  fs.readFile("./static/image/두더지게임배경.jpg", function(err,data){
+    if(err){
+      console.log("두더지게임배경화면을 불러오지 못했습니다.")
+    } else{
+      response.writeHead(200, contentimgType)
+      response.end(data)
+      }
+    })
+  }
+
+  if(request.url === "/hmoongi94image2"){
+    fs.readFile("./static/image/디그다.png", function(err,data){
+      if(err){
+        console.log("디그다사진을 불러오지 못했습니다.")
+      } else{
+        response.writeHead(200, contentimgType2)
+        response.end(data)
+      }
+    })
+  }
+
+  if(request.url === "/hmoongi94image3"){
+    fs.readFile("./static/image/뿅망치.png",function(err,data){
+      if(err){
+        console.log("뿅망치사진을 불러오지 못했습니다.")
+      } else{
+        response.writeHead(200, contentimgType2)
+        response.end(data)
+      }
+    })
+  }
+
+  else {
     response.writeHead(404, contentType);
     response.end('error:404')
   }
+
 })
+
 
 server.listen(8080,function(){
   console.log('http://localhost:8080/hmoongi94 서버가 가동중입니다. 끄려면 Ctrl+C를 누르세요.')
