697a8bdf85f0edc6443b563371fdaf5253a75e51
HongMoonGI <hmoongi94@gmail.com>
2023-10-13 12:19:26 +0900

    서버가동 완료

--- app.js
+++ app.js
@@ -1,17 +1,27 @@
 const http = require("http")
 const fs = require("fs")
-const { request } = require("http")
 
-http.createServer(function(requset,response){
+const contentType = {
+  'Content-Type': 'text/html',
+  'charset': 'utf-8',
+}
+
+const server = http.createServer(function(request,response){
+
   if(request.method !== "GET"){
     throw new Error("해당서버는 get요청처리만 가능하게끔 만들어졌습니다.")
   }
   if(request.url === "/hmoongi94"){
-    response.writeHead(200, "utf-8", {"Content-Type":"text/html"});
-    response.end(두더지잡기게임);
+    response.writeHead(200, contentType);
+    response.end();
   }
   
-  const getRoute = (requestUrl) => {
-
+  else{
+    response.writeHead(404, contentType);
+    response.end('error:404')
   }
+})
+
+server.listen(8080,function(){
+  console.log('localhost:8080/hmoongi94 서버가 가동중입니다. 끄려면 Ctrl+C를 누르세요.')
 })
