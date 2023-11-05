1d4ea0b43f71cafd008370fcf627561bbdbb45be
hongmoongi <hmoongi94@gmail.com>
2023-11-05 22:32:35 +0900

    사이드바 메뉴 토글 주석처리

--- exam_11week_Web-application/public/static/module/index.js
+++ exam_11week_Web-application/public/static/module/index.js
@@ -2,6 +2,8 @@
 
 export const load = () => {
   document.addEventListener("DOMContentLoaded", function () {
+
+    // 사이드바 메뉴 토글
     const sidebartoggle = document.getElementById("sidebarToggle");
     const sidebarmenu = document.getElementById("sidebarMenu");
 
