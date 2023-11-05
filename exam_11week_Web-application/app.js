72fd1f5cc468120362bb7044773832686dece76f
hongmoongi <hmoongi94@gmail.com>
2023-11-06 02:37:01 +0900

    json데이터 값 넣고 가져오는 연습

--- exam_11week_Web-application/app.js
+++ exam_11week_Web-application/app.js
@@ -6,8 +6,40 @@ const port = 5001;
 // 정적파일 제공을 위한 미들웨어 설정
 app.use(express.static(path.join(__dirname, './public/static')));
 // body-parser 미들웨어 설정
-// json파일에 넣을 데이터 파싱하기?
+// 클라이언트에서 보낸 JSON 데이터를 요청 객체의 req.body에 파싱된 JavaScript 객체로 저장합니다.
 app.use(express.json())
+// app.use(bodyParser.json()); // app.use(express.json)과 같은 역할을 한다.
+
+app.post('/submit', (req, res) => {
+  const newData = req.body.data; // 클라이언트에서 보낸 데이터
+
+  // 기존 JSON 파일에서 데이터 읽기
+  let existingData = {};
+  try {
+      const questionfileData = fs.readFileSync('questionData.json', 'utf8');
+      existingData = JSON.parse(questionfileData);
+  } catch (error) {
+      console.error(error);
+  }
+
+  // 새 데이터를 배열에 추가
+  existingData.push(newData);
+  // JSON 파일에 데이터 쓰기
+  fs.writeFileSync('questionData.json', JSON.stringify(existingData), 'utf8');
+
+  // 다른 JSON 파일에서 데이터 읽기
+  let responseData = {};
+  try {
+      const answerFileData = fs.readFileSync('answerData.json', 'utf8');
+      responseData = JSON.parse(answerFileData);
+  } catch (error) {
+      console.error(error);
+  }
+
+  // 클라이언트에 응답
+  res.json({ message: `서버에서 받은 데이터: ${newData}`, answerData: responseData });
+});
+
 
 
 // 서버 가동
--- exam_11week_Web-application/public/static/module/index.js
+++ exam_11week_Web-application/public/static/module/index.js
@@ -6,8 +6,35 @@ export const load = () => {
     // 사이드바 메뉴 토글
     const sidebartoggle = document.getElementById("sidebarToggle");
     const sidebarmenu = document.getElementById("sidebarMenu");
-
     addToggleClassOnClick(sidebartoggle, sidebarmenu, "close")
+
+
+    // input form 데이터값 처리하기
+    document.getElementById('questionData').addEventListener('submit', async (e) => {
+      // e.preventDefault()를 사용하여 기본 제출 동작을 중지하고, 입력된 데이터를 변수 searchContent에 저장합니다.
+      e.preventDefault();
+      const questionContent = document.getElementById('questionContent').value;
+  
+      // 서버로 데이터를 보내는 POST 요청
+      // fetch를 사용하여 "/submit" 경로로 POST 요청을 보내고, 요청 본문에 JSON 형식으로 { data: questionContent }를 전달합니다.
+      const response = await fetch('/submit', {
+          method: 'POST',
+          headers: {
+              'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({ data: questionContent }),
+      });
+  
+      if (response.ok) {
+          // 서버에서 반환한 JSON 데이터를 처리
+          // 서버로부터의 응답을 처리하기 위해 await를 사용하여 응답을 대기합니다.
+          const responseData = await response.json();
+          console.log(responseData.message);
+      } else {
+          console.error('요청 실패');
+      }
+  });
+
 
     })
   }
