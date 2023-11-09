af7102a53054c85d21091b375e4586baf70960f4
HongMoonGI <hmoongi94@gmail.com>
2023-11-09 13:13:38 +0900

    코드 중간 저장

--- exam_11week_Web-application/public/route/readfile.js
+++ exam_11week_Web-application/public/route/readfile.js
@@ -1,12 +1,18 @@
 const fs = require('fs')
 const path = require('path')
 
-function readFileAndParseASync (fileName){
+function readFileAndParseASync (fileName,callback){
   fs.readFile(path.join(__dirname, `../data/${fileName}`),'utf8',(err,filedata)=>{
     if(err){
       throw new Error(`${fileName}을 읽지 못하였습니다.`)
     } else{
-      JSON.parse(filedata)
+      try {
+        const parsedData = JSON.parse(filedata);
+        // * parsingdata를 fs.writefile을 할 콜백함수
+        callback(parsedData);
+      } catch (parseError) {
+        throw new Error(`데이터 파싱 오류: ${parseError.message}`);
+      }
     }
   })
   
