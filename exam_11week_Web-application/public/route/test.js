cec129c4c846c31d3f79a16a787343723c2a8187
HongMoonGI <hmoongi94@gmail.com>
2023-11-10 18:06:08 +0900

    함수가 문제임. 수정해야함

---
+++ exam_11week_Web-application/public/route/test.js
@@ -0,0 +1,29 @@
+const fs = require('fs')
+const path = require('path')
+
+function readFileAndParseASync(fileName, datapath, data, cookDataFunc){
+  datapath = path.join(__dirname, `../data/${fileName}`)
+  fs.readFile(datapath,'utf8',(err,filedata)=>{
+    if(err){
+      throw new Error(`${fileName}을 읽지 못하였습니다.`)
+    } else{
+      try {
+        data = JSON.parse(filedata);
+      
+        // * parsingdata를 fs.writefile을 할 콜백함수
+        cookDataFunc()
+
+      } catch (parseError) {
+        throw new Error(`데이터 파싱 오류: ${parseError.message}`);
+      }
+    }
+  })
+  
+}
+let datapath = '' 
+let data = ''
+const result = function(data){
+  return(data)
+}
+
+console.log(readFileAndParseASync('questionData.json', '', {}, result))
