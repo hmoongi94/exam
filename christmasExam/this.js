49f2b93673579d916449291492f126236ccd51d9
HongMoonGI <hmoongi94@gmail.com>
2023-12-22 18:09:13 +0900

    test: this.js파일 생성 후 this 테스트해보기

---
+++ christmasExam/this.js
@@ -0,0 +1,14 @@
+class thisPrac{
+  constructor(){
+    this.initData
+  }
+
+  initData = 
+  {
+    baseArray: ['피카츄','라이츄','꼬부기'],
+    specialProblems:[10,16,17,18,19,20],
+    htmlTagProblems:[22,23,25,27,29,30],
+    startProblemIndex: 1,
+    endProblemIndex: 30
+  }
+}
